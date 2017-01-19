ef03e76659a3e020a77012de98b25722a6911ef5
Kenny <cnkuan@qq.com>
2017-01-19 23:08:38 +0800

    170119

---
+++ 170119/task.js
@@ -0,0 +1,11 @@
+// Write a node server, get json data from files/scopes.json
+// display the fist object in the index.html
+// Almost code is finished...
+
+
+
+// Login with username and possword in index.html, and go to validation.html, display the username and password
+// in the html
+
+
+
